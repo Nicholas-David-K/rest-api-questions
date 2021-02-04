25b1d5404bf502cf9ad45ee46688d932f7b9dbc8
Nicholas David <onedavid.nicholas@gmail.com>
2021-02-04 11:29:48 +0300

    Env Variables

--- questions/settings.py
+++ questions/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q42j3bnt%0fs@p2ice$yy-2=#%#+$w@zetgwr7wrsd_5#u&eac'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flutter-questions.herokuapp.com']
 
 
 # Application definition
