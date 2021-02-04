374e3073d3cb82c57eb56e69ad4b5c18f4d46b68
Nicholas David <onedavid.nicholas@gmail.com>
2021-02-04 16:40:25 +0300

    added superuser fields

--- users/models.py
+++ users/models.py
@@ -28,8 +28,8 @@ class UserManager(BaseUserManager):
         return user
 
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        user = self.create_user(email, password)
+    def create_superuser(self, first_name, last_name, email, gender,password=None, **extra_fields):
+        user = self.create_user(first_name=first_name, last_name=last_name, email=email, gender=gender, password=password)
         
         user.is_superuser = True
         user.is_staff = True
