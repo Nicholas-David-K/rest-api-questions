92c7e4036e32e4404fe5bc6c5c4363b9ed34f57b
Nicholas David <onedavid.nicholas@gmail.com>
2021-02-04 13:30:50 +0300

    static files

--- questions/settings.py
+++ questions/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '55d07f80ef6361963525796cd5944fecfa1d5b9349c77083fa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
@@ -144,10 +145,10 @@ ACCOUNT_USER_EMAIL_FIELD = 'email'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'questions/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
