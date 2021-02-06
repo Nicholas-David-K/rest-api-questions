f585493b9c934ed482d55740c5b6d7831245d640
Nicholas David <onedavid.nicholas@gmail.com>
2021-02-06 11:23:30 +0300

    Remove Pagination

--- questions/settings.py
+++ questions/settings.py
@@ -182,10 +182,8 @@ REST_FRAMEWORK = {
     ),
 
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
-
-
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 2,
 
 }
 
