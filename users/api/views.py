5595abafffd6c47b8bd76a90b177529710b9ba67
Nicholas David <onedavid.nicholas@gmail.com>
2021-02-09 18:49:27 +0300

    Added Extra data for Token Response

--- users/api/views.py
+++ users/api/views.py
@@ -5,6 +5,8 @@ from .permissions import IsOwnerOrReadOnly
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
 
@@ -18,4 +20,14 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 class UserLoginAPIView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
-
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'email': user.email,
+            'first_name': user.first_name,
+        })
